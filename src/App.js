a00574ede2d36a370ab5b28539a630b0ec5e8fe5
Landon Whitesides <whitesideslandon@gmail.com>
2021-01-05 10:03:06 -0700

    app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import {
+  Form,
+  Header,
+  Container,
+  List,
+  Input,
+  Segment,
+} from "semantic-ui-react";
+const App = () => {
+  const [name, setName] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([name, ...todos]);
+    setName("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Segment textAlign="center">
+        <Header as="h3" textAlign="center">
+          Todo List
+        </Header>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form>
+        <List>
+          {todos.map((t, i) => (
+            <List.Item key={i}>{t}</List.Item>
+          ))}
+        </List>
+      </Segment>
+    </Container>
   );
-}
-
+};
 export default App;
